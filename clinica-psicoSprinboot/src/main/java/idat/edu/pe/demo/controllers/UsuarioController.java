8fc0992fc57d1e2e8bf8a9513b89097df9101ef6
Bailon18 <paucarmontebailon@gmail.com>
2022-11-17 23:32:50 -0500

    Token agregado - Cesar A

--- clinica-psicoSprinboot/src/main/java/idat/edu/pe/demo/controllers/UsuarioController.java
+++ clinica-psicoSprinboot/src/main/java/idat/edu/pe/demo/controllers/UsuarioController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import idat.edu.pe.demo.models.entity.Usuario;
 import idat.edu.pe.demo.models.service.IUsuariosService;
 
+//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
 //@RestController
 @Controller
 @RequestMapping(value = "/usuario")
