1bff98a297bdbbd7b28a4af09225f3c04e616461
Bailon18 <paucarmontesbailon@gmail.com>
2022-12-12 12:21:08 -0500

    se actualizo los tipo de datos222    -BAILON

--- BaseDatos/llenadoModuloUsuarios.sql
+++ BaseDatos/llenadoModuloUsuarios.sql
@@ -9,11 +9,13 @@ INSERT INTO usuarios(dni,nombres,apellidos,correo,contrasena, sexo)VALUES("11111
 INSERT INTO usuarios(dni,nombres,apellidos ,correo, contrasena ,sexo)VALUES("22222222", "Adrian", "Checa", "checa@gmail.com", "checa133", "Masculino");
 INSERT INTO usuarios(dni,nombres,apellidos ,correo, contrasena ,sexo)VALUES("33333333", "Herlly", "Arteaga Sedano", "herlly@gmail.com", "admin122", "Masculino");
 
+
+
 -- insert Permisos
 INSERT INTO permisos(usuarios_id, roles_id)VALUES(1, 1);
-INSERT INTO permisos(usuarios_id, roles_id)VALUES(1, 2);
-INSERT INTO permisos(usuarios_id, roles_id)VALUES(1, 3);
-INSERT INTO permisos(usuarios_id, roles_id)VALUES(2, 3);
+INSERT INTO permisos(usuarios_id, roles_id)VALUES(2, 2);
+INSERT INTO permisos(usuarios_id, roles_id)VALUES(3, 3);
+
 
 
 INSERT INTO ocupacion(descripcion)VALUES('sin ocupacion');
@@ -43,10 +45,53 @@ VALUES('Xiomara', 'Noriega Noriega', 'xiomara@gmail.com', 'av cesar vallejo', 'S
 INSERT INTO pacientes(nombre, apellidos, correo, direccion, distrito, documento, fechanacimiento, sexo, telefono, estadocivil, ocupacion) 
 VALUES('Lucas', 'Perez Luna', 'lucas@gmail.com', 'av republica peru', 'villa maria', '88482843', '2016-02-02', 'M', '9928838823', 1,3);
 
+
+
+
+INSERT INTO pacientes(nombre, apellidos, correo, direccion, distrito, documento, fechanacimiento, sexo, telefono, estadocivil, ocupacion) 
+VALUES('Adrian', 'Aroni', 'adrian@gmail.com', '3 de octubre', 'villa el salvador', '98764732', '1994-07-03', 'M', '98273645', 2,2);
+
+INSERT INTO pacientes(nombre, apellidos, correo, direccion, distrito, documento, fechanacimiento, sexo, telefono, estadocivil, ocupacion) 
+VALUES('Kevin', 'Albornoz', 'kevin@gmail.com', 'av cesar vallejo', 'San juan de miraflores', '87654385', '1996-10-10', 'F', '982737485', 1,4);
+
+INSERT INTO pacientes(nombre, apellidos, correo, direccion, distrito, documento, fechanacimiento, sexo, telefono, estadocivil, ocupacion) 
+VALUES('Mariabel', 'Sullca Huaman', 'mariabel@gmail.com', 'av republica peru', 'villa maria', '12239495', '2016-02-02', 'M', '999283834', 1,3);
+
+
+INSERT INTO pacientes(nombre, apellidos, correo, direccion, distrito, documento, fechanacimiento, sexo, telefono, estadocivil, ocupacion) 
+VALUES('Paola', 'Noriega', 'paola@gmail.com', '3 de octubre', 'villa el salvador', '92828345', '1994-07-03', 'M', '9999928383', 2,2);
+
+INSERT INTO pacientes(nombre, apellidos, correo, direccion, distrito, documento, fechanacimiento, sexo, telefono, estadocivil, ocupacion) 
+VALUES('Graciela', 'Pizarro Martinez', 'graciela@gmail.com', 'av cesar vallejo', 'San juan de miraflores', '9928383', '1996-10-10', 'F', '998282834', 1,4);
+
+INSERT INTO pacientes(nombre, apellidos, correo, direccion, distrito, documento, fechanacimiento, sexo, telefono, estadocivil, ocupacion) 
+VALUES('Alexis', 'Cruz Champi', 'alexis@gmail.com', 'av republica peru', 'villa maria', '22238384', '2016-02-02', 'M', '982828394', 1,3);
+
 INSERT INTO citas(estadocita, fechacita, horacita, modalidad, nota, paciente, psicologo, servicio)
 VALUES('pendiente', '2022-12-08', 9, 'Virtual', 'Se atendio con el paciente x',2, 3, 1 );
 INSERT INTO citas(estadocita, fechacita, horacita, modalidad, nota, paciente, psicologo, servicio)
 VALUES('pendiente', '2022-12-08', 12, 'Virtual', 'Se atendio con el paciente x',1, 3, 1 );
 INSERT INTO citas(estadocita, fechacita, horacita, modalidad, nota, paciente, psicologo, servicio)
 VALUES('pendiente', '2022-12-08', 18, 'Virtual', 'Se atendio con el paciente x',3, 3, 1 );
+
+INSERT INTO citas(estadocita, fechacita, horacita, modalidad, nota, paciente, psicologo, servicio)
+VALUES('pendiente', '2022-12-09', 9, 'Virtual', 'Se atendio con el paciente x',2, 3, 1 );
+INSERT INTO citas(estadocita, fechacita, horacita, modalidad, nota, paciente, psicologo, servicio)
+VALUES('pendiente', '2022-12-09', 12, 'Virtual', 'Se atendio con el paciente x',1, 3, 1 );
+INSERT INTO citas(estadocita, fechacita, horacita, modalidad, nota, paciente, psicologo, servicio)
+VALUES('pendiente', '2022-12-09', 18, 'Virtual', 'Se atendio con el paciente x',3, 3, 1 );
+
+
+INSERT INTO citas(estadocita, fechacita, horacita, modalidad, nota, paciente, psicologo, servicio)
+VALUES('pendiente', '2022-12-10', 9, 'Virtual', 'Se atendio con el paciente x',2, 3, 1 );
+INSERT INTO citas(estadocita, fechacita, horacita, modalidad, nota, paciente, psicologo, servicio)
+VALUES('pendiente', '2022-12-10', 12, 'Virtual', 'Se atendio con el paciente x',1, 3, 1 );
+INSERT INTO citas(estadocita, fechacita, horacita, modalidad, nota, paciente, psicologo, servicio)
+VALUES('pendiente', '2022-12-10', 18, 'Virtual', 'Se atendio con el paciente x',3, 3, 1 );
+
+
+
+select DISTINCT day(fechacita) from citas where month(fechacita) = 12
+
+
 
